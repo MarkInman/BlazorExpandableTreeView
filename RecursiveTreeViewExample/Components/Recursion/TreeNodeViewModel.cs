a6fc04b0b9efbf87bb4094451c213da69f61301c
Mark Inman <mark.d.inman@gmail.com>
2024-01-21 22:53:37 +0200

    Add project files.

---
+++ RecursiveTreeViewExample/Components/Recursion/TreeNodeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RecursiveTreeViewExample.Components.Recursion;
+
+public class TreeNodeViewModel
+{
+    public int TreeNodeId { get; set; }
+    public string LinkText { get; set; } = string.Empty;
+    public List<TreeNodeViewModel> Nodes { get; set; } = new List<TreeNodeViewModel>();
+}
